a695e69c8c834899fd14c3dfff61457b47bd0562
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-05-10 09:59:16 -0300

    Configuring Kotlin Activity to work with AndroidAnnotations

--- app/src/main/java/br/com/luisfernandez/github/client/HomeActivity.kt
+++ app/src/main/java/br/com/luisfernandez/github/client/HomeActivity.kt
@@ -3,15 +3,15 @@ package br.com.luisfernandez.github.client
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_home.*
+import org.androidannotations.annotations.AfterViews
+import org.androidannotations.annotations.EActivity
 
+@EActivity(R.layout.activity_home)
 class HomeActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+    @AfterViews
+    fun afterViews() {
         setSupportActionBar(toolbar)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
