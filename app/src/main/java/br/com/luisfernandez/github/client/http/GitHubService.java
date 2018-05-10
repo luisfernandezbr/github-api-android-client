81ff21516684efd803671576156feefc1dc1f908
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-05-10 12:02:03 -0300

    Changing GitHubService class to work with RX

--- app/src/main/java/br/com/luisfernandez/github/client/http/GitHubService.java
+++ app/src/main/java/br/com/luisfernandez/github/client/http/GitHubService.java
@@ -1,17 +1,20 @@
 package br.com.luisfernandez.github.client.http;
 
+import br.com.luisfernandez.github.client.http.annotations.URL;
 import br.com.luisfernandez.github.client.model.RepoListResponse;
 import br.com.luisfernandez.github.client.model.Result;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import rx.Observable;
 
 /**
  * Created by luisfernandez on 10/05/18.
  */
 
+@URL("https://api.github.com")
 public interface GitHubService
 {
     @GET("/search/repositories?q=language:Java&sort=stars&page=1")
-    Call<RepoListResponse> listRepos();
+    Observable<RepoListResponse> listRepos();
 }
