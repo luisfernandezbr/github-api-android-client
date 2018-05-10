ec7615cd22d09e83b054bbecc836af0be0b2e285
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-05-10 08:51:30 -0300

    First commit

---
+++ app/src/main/java/br/com/luisfernandez/github/client/HomeActivity.kt
@@ -0,0 +1,21 @@
+package br.com.luisfernandez.github.client
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
