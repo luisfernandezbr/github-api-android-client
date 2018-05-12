1bc14d661b02d977a165884ea4b1bbf4c3ef4cd7
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-05-12 17:19:12 -0300

    Handling errors in a specific class, before to send response to caller. (NEEDS REFACTOR)

--- app/src/main/java/br/com/luisfernandez/github/client/CallbackWrapper.java
+++ app/src/main/java/br/com/luisfernandez/github/client/CallbackWrapper.java
@@ -1,15 +1,28 @@
 package br.com.luisfernandez.github.client;
 
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.jakewharton.retrofit2.adapter.rxjava2.HttpException;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+
 import io.reactivex.observers.DisposableObserver;
+import okhttp3.ResponseBody;
 
 /**
  * Created by luisfernandez on 12/05/18.
  */
-
-public abstract class CallbackWrapper<SUCCESS> extends DisposableObserver<SUCCESS>
+public abstract class CallbackWrapper<SUCCESS, ERROR> extends DisposableObserver<SUCCESS>
 {
-    public CallbackWrapper()
+    private static final String TAG = "CallbackWrapper";
+
+    private Class<ERROR> errorClass;
+
+    public CallbackWrapper(Class<ERROR> errorClass)
     {
+        this.errorClass = errorClass;
     }
 
     @Override
@@ -18,17 +31,39 @@ public abstract class CallbackWrapper<SUCCESS> extends DisposableObserver<SUCCES
     }
 
     @Override
-    public void onError(Throwable t) {
+    public void onError(Throwable e) {
 
-        onProblem(new ErrorWrapper(t));
+        if (e instanceof HttpException) {
+            ResponseBody responseBody = ((HttpException)e).response().errorBody();
+            try
+            {
+                ERROR error = new Gson().fromJson(responseBody.string(), errorClass);
+                ServerError<ERROR> errorServerError = new ServerError<>(error, ((HttpException) e).response().code(), null);
+                onError(errorServerError);
+            }
+            catch (IOException e1)
+            {
+                ServerError<ERROR> errorServerError = new ServerError<>(null, 5000, "Generic Error");
+                onError(errorServerError);
+            }
+        } else if (e instanceof SocketTimeoutException) {
+            ServerError<ERROR> errorServerError = new ServerError<>(null, 3000, ((SocketTimeoutException) e).getMessage());
+            onError(errorServerError);
+        } else if (e instanceof IOException) {
+            ServerError<ERROR> errorServerError = new ServerError<>(null, 4000, ((IOException) e).getMessage());
+            onError(errorServerError);
+        } else {
+            ServerError<ERROR> errorServerError = new ServerError<>(null, 5000, "Generic Error");
+            onError(errorServerError);
+        }
     }
 
     @Override
     public void onComplete() {
-
+        Log.d(TAG, "onComplete not implemented!");
     }
 
-    protected abstract void onSuccess(SUCCESS success);
+    abstract void onSuccess(SUCCESS success);
 
-    protected abstract void onProblem(ErrorWrapper errorWrapper);
+    abstract void onError(ServerError<ERROR> error);
 }
