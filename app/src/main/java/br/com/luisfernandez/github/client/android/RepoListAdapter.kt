f38d94f83a08bed133732457fd51615a20a2f2e8
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-05-12 20:29:37 -0300

    Changing Adapter to work with pagination  (Solution based on https://github.com/Suleiman19/Android-Pagination-with-RecyclerView)

--- app/src/main/java/br/com/luisfernandez/github/client/android/RepoListAdapter.kt
+++ app/src/main/java/br/com/luisfernandez/github/client/android/RepoListAdapter.kt
@@ -1,49 +1,109 @@
 package br.com.luisfernandez.github.client.android
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.luisfernandez.github.client.R
 import br.com.luisfernandez.github.client.misc.ImageLoader
 import br.com.luisfernandez.github.client.model.Repo
+import kotlinx.android.synthetic.main.item_loading_more.view.*
 import kotlinx.android.synthetic.main.item_repository_list.view.*
+
+import kotlin.collections.ArrayList
 
 /**
  * Created by luisfernandez on 10/05/18.
  */
-class RepoListAdapter(private val repoList: List<Repo>) :
-        RecyclerView.Adapter<RepoListAdapter.ViewHolder>()
+class RepoListAdapter(private val repoList: ArrayList<Repo> = ArrayList()) :
+        RecyclerView.Adapter<RecyclerView.ViewHolder>()
 {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_repository_list, parent, false)
-        return ViewHolder(view)
+    companion object {
+        const val TAG = "RepoListAdapter"
+        const val CONTENT = 1
+        const val FOOTER = 2
+    }
+
+    private var isLoadingAdded = false
+
+    override fun getItemViewType(position: Int): Int {
+        if (isLoadingAdded && position == this.repoList.size) {
+            Log.d(TAG, "getItemViewType(): $FOOTER")
+            return FOOTER
+        } else {
+            Log.d(TAG, "getItemViewType(): $CONTENT")
+            return CONTENT
+        }
     }
 
     override fun getItemCount(): Int {
-        return this.repoList.size
+        return if (isLoadingAdded) this.repoList.size + 1 else this.repoList.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val repo = repoList[position]
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        Log.d(TAG, "onCreateViewHolder() itemType: $viewType")
 
-        holder.textRepoName.text = repo.name
-        holder.textRepoDescription.text = repo.description
-        holder.textRepoAuthor.text = repo.owner.login
-        holder.textStartCount.text = repo.stargazersCount.toString()
-        holder.textForksCount.text = repo.forksCount.toString()
-
-        ImageLoader.loadImage(repo.owner.avatarUrl, holder.imageRepoAuthor)
+        return when(viewType) {
+            CONTENT -> ContentViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_repository_list, parent, false))
+            else -> FooterViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_loading_more, parent, false))
+        }
     }
 
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+        val itemViewType = getItemViewType(position)
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textRepoName = itemView.textRepoName
-        val textRepoDescription = itemView.textRepoDescription
-        val textRepoAuthor = itemView.textRepoAuthor
-        val imageRepoAuthor = itemView.imageRepoAuthor
-        val textStartCount = itemView.textStartCount
-        val textForksCount = itemView.textForksCount
+        when (itemViewType) {
+            CONTENT -> {
+                val holderContent = viewHolder as ContentViewHolder
+
+                Log.d(TAG, "listSize: ${repoList.size}")
+
+                val repo = getItem(position)
+                holderContent.textRepoName.text = repo.name
+                //holderContent.textRepoDescription.text = repo.description
+                holderContent.textRepoAuthor.text = repo.owner.login
+                holderContent.textStartCount.text = repo.stargazersCount.toString()
+                holderContent.textForksCount.text = repo.forksCount.toString()
+
+                ImageLoader.loadImage(repo.owner.avatarUrl, holderContent.imageRepoAuthor)
+            }
+//            else -> {
+//                val holderContent = viewHolder as FooterViewHolder
+//            }
+        }
     }
 
+    private fun getItem(position: Int): Repo {
+        return this.repoList[position]
+    }
+
+    fun showFooter() {
+        Log.d(TAG, "showFooter()")
+        isLoadingAdded = true
+        notifyItemInserted(itemCount)
+    }
+
+    fun addAll(repoList: List<Repo>) {
+        isLoadingAdded = false
+        notifyItemInserted(itemCount)
+
+        this.repoList.addAll(repoList)
+        isLoadingAdded = false
+        notifyDataSetChanged()
+    }
+
+    class ContentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textRepoName = itemView.textRepoName!!
+        val textRepoDescription = itemView.textRepoDescription!!
+        val textRepoAuthor = itemView.textRepoAuthor!!
+        val imageRepoAuthor = itemView.imageRepoAuthor!!
+        val textStartCount = itemView.textStartCount!!
+        val textForksCount = itemView.textForksCount!!
+
+    }
+    class FooterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val progressBar = itemView.progressBar!!
+
+    }
 }
