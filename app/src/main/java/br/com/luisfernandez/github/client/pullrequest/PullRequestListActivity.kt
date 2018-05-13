59a38e9cc7802214eea6f0890b792c2ac3621d13
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-05-12 21:40:57 -0300

    Creating PullRequestListActivity

---
+++ app/src/main/java/br/com/luisfernandez/github/client/pullrequest/PullRequestListActivity.kt
@@ -0,0 +1,79 @@
+package br.com.luisfernandez.github.client.pullrequest
+
+import android.annotation.SuppressLint
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
+import br.com.luisfernandez.github.client.GitHubErrorBody
+import br.com.luisfernandez.github.client.PaginationScrollListener
+import br.com.luisfernandez.github.client.R
+import br.com.luisfernandez.github.client.android.RepoListAdapter
+import br.com.luisfernandez.github.client.extensions.setGone
+import br.com.luisfernandez.github.client.extensions.setVisible
+import br.com.luisfernandez.github.client.http.ServerError
+import br.com.luisfernandez.github.client.model.PullRequestResponse
+import br.com.luisfernandez.github.client.model.Repo
+import br.com.luisfernandez.github.client.repolist.RepoListView
+import kotlinx.android.synthetic.main.activity_repo_list.*
+import org.androidannotations.annotations.AfterViews
+import org.androidannotations.annotations.EActivity
+
+
+
+@SuppressLint("Registered")
+@EActivity(R.layout.activity_repo_list)
+class PullRequestListActivity : AppCompatActivity(), PullRequestListView {
+
+    private lateinit var repoListAdapter: RepoListAdapter
+    private lateinit var presenter: PullRequestPresenter
+
+    override fun handleError(serverError: ServerError<GitHubErrorBody>) {
+
+        if (serverError.errorBody != null) {
+            Toast.makeText(this, "HTTP STATUS: ${serverError.httpStatus}\nMESSAGE${serverError.errorBody!!.message} ", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "HTTP STATUS: ${serverError.httpStatus}\nMESSAGE${serverError.errorMessage} ", Toast.LENGTH_LONG).show()
+        }
+
+        var progressBar = progressBar
+        progressBar.setGone()
+    }
+
+    override fun showError() {
+        Toast.makeText(this, "ERRORRRRR", Toast.LENGTH_LONG).show()
+
+        var progressBar = progressBar
+        progressBar.setGone()
+    }
+
+    override fun showProgress() {
+        var progressBar = progressBar
+        progressBar.setVisible()
+
+        var recyclerView = recyclerView
+        recyclerView.setGone()
+    }
+
+    @AfterViews
+    fun afterViews() {
+        presenter = PullRequestPresenterImpl()
+        presenter.inject(this)
+
+        val recyclerView = recyclerView
+        val layoutManager = LinearLayoutManager(this)
+        repoListAdapter = RepoListAdapter()
+
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = repoListAdapter
+
+
+        presenter.loadPullRequestList("", "")
+    }
+
+    override fun showPullRequestList(repoList: List<PullRequestResponse>) {
+        val progressBar = progressBar
+        progressBar.setGone()
+        recyclerView.setVisible()
+    }
+}
