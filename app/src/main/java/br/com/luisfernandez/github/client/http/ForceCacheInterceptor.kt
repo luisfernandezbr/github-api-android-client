138521673ff908d35bf44d4ffd9001ce743d1058
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-05-13 13:50:55 -0300

    Adding Interceptor to enable caching

---
+++ app/src/main/java/br/com/luisfernandez/github/client/http/ForceCacheInterceptor.kt
@@ -0,0 +1,32 @@
+package br.com.luisfernandez.github.client.http
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+/**
+ * Solution from https://gist.github.com/polbins/1c7f9303d2b7d169a3b1
+ * https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.9.4
+ */
+class ForceCacheInterceptor @Inject constructor() : Interceptor
+{
+    override fun intercept(chain: Interceptor.Chain?): Response
+    {
+        val request = chain!!.request()
+
+        if (request.method().equals("GET"))
+        {
+            request.newBuilder()
+                    .header("Cache-Control", "public, only-if-cached, max-stale=2419200")
+                    .build()
+        }
+
+        val response = chain.proceed(request)
+
+        // Re-write response CC header to force use of cache
+        return response.newBuilder()
+                .removeHeader("Cache-Control")
+                .header("Cache-Control", "public, max-age=86400") // 1 day
+                .build()
+    }
+}
