af3566a8e0fd17e597e892d58988678ee8effa36
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-05-14 00:16:48 -0300

    Deleting duplicated JSON mapping POJO class

--- app/src/main/java/br/com/luisfernandez/github/client/model/Owner.kt
+++
@@ -1,7 +0,0 @@
-package br.com.luisfernandez.github.client.model
-
-import com.google.gson.annotations.SerializedName
-data class Owner(
-    @SerializedName("login") val login: String,
-    @SerializedName("avatar_url") val avatarUrl: String
-)
--- app/src/main/java/br/com/luisfernandez/github/client/model/Repo.kt
+++ app/src/main/java/br/com/luisfernandez/github/client/model/Repo.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class Repo(
     @SerializedName("name") val name: String,
     @SerializedName("description") val description: String,
-    @SerializedName("owner") val owner: Owner,
+    @SerializedName("owner") val owner: User,
     @SerializedName("full_name") val fullName: String,
     @SerializedName("stargazers_count") val stargazersCount: Int,
     @SerializedName("watchers_count") val watchersCount: Int,
