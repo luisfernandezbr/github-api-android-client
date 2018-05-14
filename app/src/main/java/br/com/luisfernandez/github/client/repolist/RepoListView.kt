c9fa38fed2eedb6ebec9c9e4b5f0c1cec2296916
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-05-13 23:46:00 -0300

    Changing hierarchy

--- app/src/main/java/br/com/luisfernandez/github/client/repolist/RepoListView.kt
+++ app/src/main/java/br/com/luisfernandez/github/client/repolist/RepoListView.kt
@@ -1,14 +1,10 @@
 package br.com.luisfernandez.github.client.repolist
 
-import br.com.luisfernandez.github.client.ErrorHandlerView
 import br.com.luisfernandez.github.client.GitHubErrorBody
+import br.com.luisfernandez.github.client.LoadContentView
 import br.com.luisfernandez.github.client.model.Repo
 
 /**
  * Created by luisfernandez on 11/05/18.
  */
-interface RepoListView : ErrorHandlerView<GitHubErrorBody> {
-    fun showRepoList(repoList: List<Repo>)
-    fun showProgress()
-    fun showError()
-}
+interface RepoListView : LoadContentView<List<Repo>, GitHubErrorBody>
