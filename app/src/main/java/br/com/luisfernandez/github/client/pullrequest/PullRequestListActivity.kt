4253c641825f6a64c3a4881d5e9d4331af279383
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-05-14 23:30:08 -0300

    Refactoring class. Reorganizing methods

--- app/src/main/java/br/com/luisfernandez/github/client/pullrequest/PullRequestListActivity.kt
+++ app/src/main/java/br/com/luisfernandez/github/client/pullrequest/PullRequestListActivity.kt
@@ -1,15 +1,17 @@
 package br.com.luisfernandez.github.client.pullrequest
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import br.com.luisfernandez.github.client.model.GitHubErrorBody
 import br.com.luisfernandez.github.client.OnItemClickListener
 import br.com.luisfernandez.github.client.R
 import br.com.luisfernandez.github.client.android.AppApplication
 import br.com.luisfernandez.github.client.extensions.setGone
 import br.com.luisfernandez.github.client.extensions.setVisible
 import br.com.luisfernandez.github.client.http.ServerError
+import br.com.luisfernandez.github.client.model.GitHubErrorBody
 import br.com.luisfernandez.github.client.model.PullRequestResponse
 import kotlinx.android.synthetic.main.activity_list.*
 import kotlinx.android.synthetic.main.view_state_empty.*
@@ -19,9 +21,6 @@ import org.androidannotations.annotations.AfterViews
 import org.androidannotations.annotations.EActivity
 import org.androidannotations.annotations.Extra
 import javax.inject.Inject
-import android.content.Intent
-import android.net.Uri
-
 
 @SuppressLint("Registered")
 @EActivity(R.layout.activity_list)
@@ -41,7 +40,6 @@ class PullRequestListActivity : AppCompatActivity(), PullRequestListView {
 
     @AfterViews
     fun afterViews() {
-        val recyclerView = recyclerView
         val layoutManager = LinearLayoutManager(this)
 
         recyclerView.layoutManager = layoutManager
@@ -52,10 +50,7 @@ class PullRequestListActivity : AppCompatActivity(), PullRequestListView {
     }
 
     override fun handleError(serverError: ServerError<GitHubErrorBody>) {
-        layoutProgress.setGone()
-        layoutEmpty.setGone()
-        layoutError.setVisible()
-        recyclerView.setGone()
+        this.showErrorState()
 
         if (serverError.errorBody != null) {
             textErrorMessage.text = serverError.errorBody!!.message
@@ -67,6 +62,13 @@ class PullRequestListActivity : AppCompatActivity(), PullRequestListView {
             showLoading()
             presenter.loadPullRequestList(owner, repoName)
         }
+    }
+
+    private fun showErrorState() {
+        layoutProgress.setGone()
+        layoutEmpty.setGone()
+        layoutError.setVisible()
+        recyclerView.setGone()
     }
 
     override fun showLoading() {
@@ -84,23 +86,36 @@ class PullRequestListActivity : AppCompatActivity(), PullRequestListView {
     }
 
     override fun showContent(content: List<PullRequestResponse>) {
-
         if (content.isEmpty()) {
             showEmpty()
         } else {
             layoutProgress.setGone()
             layoutEmpty.setGone()
             layoutError.setGone()
-            recyclerView.adapter = PullRequestListAdapter(
-                    content as ArrayList<PullRequestResponse>,
-                    onItemClickListener = object : OnItemClickListener<PullRequestResponse> {
-                        override fun onItemClick(type: PullRequestResponse) {
-                            val intent = Intent(Intent.ACTION_VIEW)
-                            intent.data = Uri.parse(type.htmlUrl)
-                            startActivity(intent)
-                        }
-                    })
             recyclerView.setVisible()
+
+            recyclerView.adapter = this.getPullRequestListAdapter(content)
         }
+    }
+
+    private fun getPullRequestListAdapter(content: List<PullRequestResponse>): PullRequestListAdapter {
+        return PullRequestListAdapter(
+                content as ArrayList<PullRequestResponse>,
+                getOnItemClickListener()
+        )
+    }
+
+    private fun getOnItemClickListener(): OnItemClickListener<PullRequestResponse> {
+        return object : OnItemClickListener<PullRequestResponse> {
+            override fun onItemClick(type: PullRequestResponse) {
+                openOnBrowser(type.htmlUrl)
+            }
+        }
+    }
+
+    private fun openOnBrowser(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
     }
 }
