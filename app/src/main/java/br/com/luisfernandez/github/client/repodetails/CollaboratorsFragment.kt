e6752799dcb1b2c490d03e33bf17fb3c19633808
Óliver Silva <oliver@whitebook.com.br>
2019-04-18 14:46:41 -0300

    Renaming XML fragment_collaborators to fragment_contributors_list and adding its content

--- app/src/main/java/br/com/luisfernandez/github/client/repodetails/CollaboratorsFragment.kt
+++ app/src/main/java/br/com/luisfernandez/github/client/repodetails/CollaboratorsFragment.kt
@@ -14,7 +14,7 @@ class CollaboratorsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_collaborators, container, false)
+        return inflater.inflate(R.layout.fragment_contributors_list, container, false)
     }
 
 
