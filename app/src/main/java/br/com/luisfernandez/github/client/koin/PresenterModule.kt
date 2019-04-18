2b93ca6fb5ca82f62040dd00fa27e3a0b6477e9a
Óliver Silva <oliver@whitebook.com.br>
2019-04-18 13:02:10 -0300

    Removing unecessary files.

--- app/src/main/java/br/com/luisfernandez/github/client/koin/PresenterModule.kt
+++ app/src/main/java/br/com/luisfernandez/github/client/koin/PresenterModule.kt
@@ -1,7 +1,5 @@
 package br.com.luisfernandez.github.client.koin
 
-import br.com.luisfernandez.github.client.repodetails.pullrequest.PullRequestPresenter
-import br.com.luisfernandez.github.client.repodetails.pullrequest.PullRequestPresenterImpl
 import br.com.luisfernandez.github.client.repolist.RepoListPresenter
 import br.com.luisfernandez.github.client.repolist.RepoListPresenterImpl
 import org.koin.dsl.module.module
@@ -9,9 +7,5 @@ import org.koin.dsl.module.module
 val presenterModule = module {
     single<RepoListPresenter> {
         RepoListPresenterImpl(get())
-    }
-
-    single<br.com.luisfernandez.github.client.repodetails.pullrequest.PullRequestPresenter> {
-        br.com.luisfernandez.github.client.repodetails.pullrequest.PullRequestPresenterImpl(get())
     }
 }
--- app/src/main/java/br/com/luisfernandez/github/client/repodetails/pullrequest/PullRequestPresenter.kt
+++
@@ -1,9 +0,0 @@
-package br.com.luisfernandez.github.client.repodetails.pullrequest
-
-/**
- * Created by luisfernandez on 12/05/18.
- */
-interface PullRequestPresenter {
-    fun loadPullRequestList(owner: String, repoName: String)
-    fun inject(pullRequestView: br.com.luisfernandez.github.client.repodetails.pullrequest.PullRequestListView)
-}
