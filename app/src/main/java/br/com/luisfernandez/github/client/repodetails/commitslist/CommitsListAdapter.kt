0669480a17c1cc4e32acb4a303749aa36a89382d
Oliver Silva <silva.liver@gmail.com>
2019-04-22 16:26:47 -0300

    Adding CommitsListAdapter to the project.

---
+++ app/src/main/java/br/com/luisfernandez/github/client/repodetails/commitslist/CommitsListAdapter.kt
@@ -0,0 +1,42 @@
+package br.com.luisfernandez.github.client.repodetails.commitslist
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.luisfernandez.github.client.R
+import br.com.luisfernandez.github.client.misc.ImageLoader
+import br.com.luisfernandez.github.client.pojo.CommitResponse
+import kotlinx.android.synthetic.main.item_commits_list.view.*
+import java.util.*
+
+/**
+ * Created by luisfernandez on 12/05/18.
+ */
+class CommitsListAdapter(
+        private val commitsList: ArrayList<CommitResponse> = ArrayList()
+) : RecyclerView.Adapter<CommitsListAdapter.CommitViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommitViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_commits_list, parent, false)
+        return CommitViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return commitsList.size
+    }
+
+    override fun onBindViewHolder(holder: CommitViewHolder, position: Int) {
+        val item = commitsList[position]
+
+        holder.textCommiterName.text = item.author.login
+        holder.textDate.text = item.date.toString()
+
+        ImageLoader.loadImage(item.author.avatarUrl, holder.imageCommiter)
+    }
+
+    class CommitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var imageCommiter = itemView.imageCommitter!!
+        var textCommiterName = itemView.textCommitterName!!
+        var textDate = itemView.textDate!!
+    }
+}
