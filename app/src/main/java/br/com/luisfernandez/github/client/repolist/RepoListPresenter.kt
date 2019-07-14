2424bfa666ad9cf431aa5be0bc7ac24178857a6b
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2019-07-13 23:12:17 -0300

    Removing old presenters

--- app/src/main/java/br/com/luisfernandez/github/client/repolist/RepoListPresenter.kt
+++
@@ -1,9 +0,0 @@
-package br.com.luisfernandez.github.client.repolist
-
-/**
- * Created by luisfernandez on 11/05/18.
- */
-interface RepoListPresenter {
-    fun loadRepoList(page: Int, language: String)
-    fun inject(repoListView: RepoListView)
-}
